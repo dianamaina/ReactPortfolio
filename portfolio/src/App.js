54834d924c300f2fa22beebc7bd760fd2b664b49
diana maina <dianamainachui@gmail.com>
2020-11-13 06:39:34 -0500

    modified app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,46 +1,22 @@
-  
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import Header from './Header.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
 
-export const ColorScheme = {
-  primary: 'red',
-  secondary: 'blue',
-  third: 'purple',
-  fourth: 'goldenrod'
-}
-
-const AppDiv = styled.div`
-  display: flex;
-  min-height: 100vh;
-  flex-direction: column;
-  font-size: 1.15em;
-`;
-
-export const CenteredHeader = styled.h4`
-  text-align: center;
-  margin-bottom: 1.25em;
-`
-
-
-const ContainerDiv = styled.div`
-  margin: 1% 17% 3% 17%;
-  flex: 1;
-  font-family: 'Open Sans', sans-serif;
-`
-
+import Home from './pages/Home.jsx';
+import Skills from './pages/Skills.jsx';
+import Contact from './pages/Contact.jsx';
 class App extends Component {
   render() {
     return (
-      <AppDiv>
-        <Header />
-        <ContainerDiv>
-          {this.props.children}
-        </ContainerDiv>
-
-      </AppDiv>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/skills" component={Skills}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App
+export default App;
